b60ba85e2a77a7b9e1d8172e0e41b90ccf2bc0fd
Rafael Felix Souza <113321175+foionova@users.noreply.github.com>
2025-08-18 16:30:50 -0300

    atualizacaio dia python 2

--- 4- quinta/aula012/aula012.py
+++ 4- quinta/aula012/aula012.py
@@ -1,12 +1,21 @@
-#CODIGO TABUADA 1 a 10
-i = 1
-while i < 11:
+'''
+#tentando deduzir a versao refinada com for
+for i in range (1,11):
     print(f"tabuada do {i}")
-    j = 1
-    while j < 11:
+    for j in range(1,11):
         print(f"{i} x {j} = {i*j}")
-        j+=1
-    i+=1   
-#i)porque i = 1?
-#porque vamo comear a tabuada do 1
-#ii) porque o j tem que estar ali?
+    print() #print aqui no final pra dar uma linha em branco no final do codigo
+
+#compreensao de len lista
+lista = [3,'danilo',0.5,True]
+lista[0] = 3
+lista[1] = 'danilo'
+lista[2] = 0.5
+lista[3] = True
+
+for i in range(len(lista)):
+    print(f"lista[{i}] = {lista[i]}")
+'''
+notas = [7, 8, 9, 10]
+for i in range(len(notas)):
+    print(f"A nota {i+1} é {notas[i]}")
