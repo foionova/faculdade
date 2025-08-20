86780b824108e50d6ab69db1315b0e647d81e3f6
foionova <rafaelfelixsouza943@gmail.com>
2025-08-20 08:11:27 -0300

    testando altercao inicial na aula de Web

--- 3- quarta/w3b dev/aula012/aula12.js
+++ 3- quarta/w3b dev/aula012/aula12.js
@@ -1,6 +1,6 @@
 //Vide: ferramenta de desenvolvimento frontend criada para ser uma solucao mais agil em comparacao ao webpack qe e amplamente utilizado pelo ecossistema javascript
 
-//Typescript: criado ela micrososft para estender o javascript com a adicao de tipos estaticos. Essa abordagem proporciona maior seguranca e robuste no desenvolvimento de software. permitindo que os desenvolvedores escrevam um codigo mais limpo e legivel 
+//Typescript: criado pela micrososft para estender o javascript com a adicao de tipos estaticos. Essa abordagem proporciona maior seguranca e robuste no desenvolvimento de software. permitindo que os desenvolvedores escrevam um codigo mais limpo e legivel 
 
 //porque o JS e fracamente tipado? ex: nao precisa colocar o type de dados, ele "automaticamente" detecta oque ta sendo inserid
 
