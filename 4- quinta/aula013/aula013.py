f08278b67edb1488ae683567ab5b227c6e0349aa
foionova <rafaelfelixsouza943@gmail.com>
2025-08-21 11:01:54 -0300

    TABULEIRO DE XADREEEEEEEEEZ

--- 4- quinta/aula013/aula013.py
+++ 4- quinta/aula013/aula013.py
@@ -30,7 +30,7 @@ for i in range(linhas):
         linha.append(1)
     matriz.append(linha)
     print(matriz)
-'''
+
 import matplotlib.pyplot as plt
 
 linhas = 3
@@ -54,9 +54,41 @@ diagonal = cria_matriz(linhas=10, colunas=10)
 for i in range(len(diagonal)):
     diagonal[i][i] = 1
 a = diagonal
-# mostra no terminal
 
 # mostra como imagem
 plt.imshow(a, cmap='hot')
 plt.colorbar()
+plt.show()
+'''
+#iv) fazer tabuleiro de xadrez
+import matplotlib.pyplot as plt
+
+linhas = 8
+colunas = 8
+
+def cria_matriz(linhas, colunas):
+    matriz = []
+    for i in range(linhas):
+        linha = []
+        for j in range(colunas):
+            linha.append(0)  # preenche com 0
+        matriz.append(linha)
+    return matriz
+
+xadrez = cria_matriz(linhas, colunas)
+
+# preencher xadrez
+for i in range(len(xadrez)):
+    for j in range(len(xadrez[0])):
+        if i%2 == 0:
+            if j%2 == 0:
+                xadrez[i][j] = 0
+            else:
+                xadrez[i][j] = 1
+        else:
+            if j%2 == 0:
+                xadrez[i][j] = 1
+            else:
+                xadrez[i][j] = 0
+plt.imshow(xadrez, cmap='gray')  # cinza para xadrez ficar preto/branco
 plt.show()
